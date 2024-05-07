f184c0c67b88fca16f140af4701e3129ba46bdd9
Amith Koujalgi <amith.koujalgi@razorthink.com>
2024-05-08 02:03:51 +0530

    Updated README.md

--- server/src/main/java/io/github/amithkoujalgi/netwatch/AppStartupRunner.java
+++ server/src/main/java/io/github/amithkoujalgi/netwatch/AppStartupRunner.java
@@ -1,7 +1,6 @@
 package io.github.amithkoujalgi.netwatch;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -9,15 +8,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class AppStartupRunner implements ApplicationRunner {
-  @Value("${netwatch.event-listener.port}")
-  private int port;
 
-  @Override
-  public void run(ApplicationArguments args) throws Exception {
-//    EventListenerServer eventListenerServer = new EventListenerServer(port);
-//    Thread eventListenerServerThread = new Thread(eventListenerServer);
-//    eventListenerServerThread.setName("event-listener");
-//    eventListenerServerThread.start();
-//    eventListenerServerThread.join();
-  }
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+    }
 }
