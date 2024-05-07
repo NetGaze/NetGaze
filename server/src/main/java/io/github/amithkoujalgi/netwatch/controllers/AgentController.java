d0f2e356edac2e174e4b1a7a09b4fc2d7cb1b00e
Amith Koujalgi <amith.koujalgi@razorthink.com>
2024-05-08 00:04:30 +0530

    updated netwatch-server with graph rendering

--- server/src/main/java/io/github/amithkoujalgi/netwatch/controllers/AgentController.java
+++ server/src/main/java/io/github/amithkoujalgi/netwatch/controllers/AgentController.java
@@ -11,15 +11,16 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 @Slf4j
 @Tag(name = "", description = "Agent APIs")
@@ -27,91 +28,53 @@ import org.springframework.web.bind.annotation.RestController;
 public class AgentController {
 
 
-  @Operation(summary = "List agents")
-  @ApiResponse(responseCode = "200", content = {
-      @Content(array = @ArraySchema(schema = @Schema(implementation = List.class, subTypes = {
-          Agent.class})), mediaType = "application/json")})
-  @GetMapping("/agents")
-  public List<Agent> agents() {
+    @Operation(summary = "List agents")
+    @ApiResponse(responseCode = "200", content = {
+            @Content(array = @ArraySchema(schema = @Schema(implementation = List.class, subTypes = {
+                    Agent.class})), mediaType = "application/json")})
+    @GetMapping("/agents")
+    public List<Agent> agents() {
 
-    Connection c = new Connection();
-    c.setName("C1");
-    c.setHost("google.com");
-    c.setPort(80);
-    c.setType(ConnectionType.HTTP);
+        Connection c = new Connection();
+        c.setName("C1");
+        c.setHost("google.com");
+        c.setPort(80);
+        c.setType(ConnectionType.HTTP);
 
-    Agent agent = new Agent();
-    agent.setName("test");
-    agent.setHost("0.1.2.3");
-    agent.setConnections(Collections.singletonList(c));
+        Agent agent = new Agent();
+        agent.setName("test");
+        agent.setHost("0.1.2.3");
+        agent.setConnections(Collections.singletonList(c));
 
-    AgentRegister.getInstance().updateAgent(agent);
-    return AgentRegister.getInstance().getAgents();
-  }
-
-
-  @Operation(summary = "Get connections of an agent")
-  @ApiResponse(responseCode = "200", content = {
-      @Content(array = @ArraySchema(schema = @Schema(implementation = List.class, subTypes = {
-          Connection.class})), mediaType = "application/json")})
-  @GetMapping("/agents/{name}")
-  public ResponseEntity<?> agents(String agentName) {
-    try {
-      List<Connection> connections = AgentRegister.getInstance().getConnections(agentName);
-      if (connections != null) {
-        return ResponseEntity.ok(connections);
-      } else {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Agent not found");
-      }
-    } catch (Exception e) {
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-          .body("An error occurred: " + e.getMessage());
+        AgentRegister.getInstance().updateAgent(agent);
+        return AgentRegister.getInstance().getAgents();
     }
-  }
 
-  @Operation(summary = "Get graph")
-  @ApiResponse(responseCode = "200", content = {
-      @Content(array = @ArraySchema(schema = @Schema(implementation = Graph.class)), mediaType = "application/json")})
-  @GetMapping("/graph")
-  public Graph getGraph() {
 
-    Agent agent1 = new Agent();
-    agent1.setName("agent1");
-    agent1.setHost("192.168.0.1");
-    agent1.setLastSeenAt(new Date());
+    @Operation(summary = "Get connections of an agent")
+    @ApiResponse(responseCode = "200", content = {
+            @Content(array = @ArraySchema(schema = @Schema(implementation = List.class, subTypes = {
+                    Connection.class})), mediaType = "application/json")})
+    @GetMapping("/agents/{name}")
+    public ResponseEntity<?> agents(String agentName) {
+        try {
+            List<Connection> connections = AgentRegister.getInstance().getConnections(agentName);
+            if (connections != null) {
+                return ResponseEntity.ok(connections);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Agent not found");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An error occurred: " + e.getMessage());
+        }
+    }
 
-    Connection c1 = new Connection();
-    c1.setName("C1");
-    c1.setHost("192.168.0.3");
-    c1.setPort(80);
-    c1.setType(ConnectionType.HTTP);
-//    c1.setLastCheckedAt(new Date());
-
-    Connection c2 = new Connection();
-    c2.setName("C2");
-    c2.setHost("192.168.0.4");
-    c2.setPort(80);
-    c2.setType(ConnectionType.HTTP);
-//    c2.setLastCheckedAt(new Date());
-
-    Agent agent2 = new Agent();
-    agent2.setName("agent2");
-    agent2.setHost("192.168.0.2");
-    agent2.setLastSeenAt(new Date());
-
-    Connection c3 = new Connection();
-    c3.setName("C3");
-    c3.setHost("192.168.0.5");
-    c3.setPort(80);
-    c3.setType(ConnectionType.HTTP);
-//    c3.setLastCheckedAt(new Date());
-
-    agent1.setConnections(Arrays.asList(c1, c2));
-    agent2.setConnections(List.of(c3));
-
-//    AgentRegister.getInstance().updateAgent(agent1);
-//    AgentRegister.getInstance().updateAgent(agent2);
-
-    return AgentRegister.getInstance().getGraph();
-  }
+    @Operation(summary = "Get graph")
+    @ApiResponse(responseCode = "200", content = {
+            @Content(array = @ArraySchema(schema = @Schema(implementation = Graph.class)), mediaType = "application/json")})
+    @GetMapping("/graph")
+    public Graph getGraph() {
+        return AgentRegister.getInstance().getGraph();
+    }
 }
--- server/src/main/resources/static/code.js
+++ server/src/main/resources/static/code.js
@@ -1,7 +1,21 @@
+// noinspection JSUnresolvedReference,JSJQueryEfficiency
+
+$(document).ready(function () {
+    $('body').append('<div id="highlighter" style="border: 1px black solid; display: inline-block;"></div>');
+});
+
+function convertToReadable(variableName) {
+    return variableName
+        .replace(/([a-z])([A-Z])/g, '$1 $2') // Add space between lowercase and uppercase letters
+        .replace(/_/g, ' ') // Replace underscores with spaces
+        .replace(/\b\w/g, c => c.toUpperCase()); // Capitalize first letter of each word
+}
+
 function render(graphData) {
-    var cy = window.cy = cytoscape({
+    let cy = window.cy = cytoscape({
         container: document.getElementById('cy'),
         boxSelectionEnabled: false,
+        userZoomingEnabled: false,
         style: [
             {
                 selector: 'node',
@@ -50,17 +64,17 @@ function render(graphData) {
                 }
             },
             {
-                  selector: '.agent',
-                  css: {
-                     'shape': 'rectangle',
-                  }
-              },
-              {
-                  selector: '.connection',
-                  css: {
-                     'shape': 'ellipse',
-                  }
-              }
+                selector: '.agent',
+                css: {
+                    'shape': 'rectangle',
+                }
+            },
+            {
+                selector: '.connection',
+                css: {
+                    'shape': 'ellipse',
+                }
+            }
         ],
         elements: graphData,
         layout: {
@@ -68,22 +82,61 @@ function render(graphData) {
             padding: 5
         }
     });
-    cy.nodes().map(x => console.log(x));
-    cy.on('mouseover', 'node', function(evt){
-      var node = evt.target;
-      console.log( node.id() );
-      evt.target.style({
-          'background-color': 'yellow'
-        });
+
+    cy.nodes().forEach(node => {
+        // console.log(graphData);
+        for (g in graphData.nodes) {
+            if (node.id() === graphData.nodes[g].data.id) {
+                node.data('additionalProperties', graphData.nodes[g].additionalProperties);
+            }
+        }
     });
-    cy.on('mouseout', 'node', function(evt){
-      var node = evt.target;
-      console.log( node.id() );
-      evt.target.style({
-          'background-color': 'gray'
+
+    cy.on('mouseover', 'node', function (evt) {
+        let node = evt.target;
+        let nodeId = node.id();
+        let nodePosition = node.position();
+        let nodeAdditionalProperties = node.data().additionalProperties;
+        // evt.target.style({
+        //     'background-color': 'yellow'
+        // });
+        // console.log(node.css().shape);
+        let text = "";
+        if (node.css().shape === 'ellipse') {
+            nodeAdditionalProperties['Connection'] = node.id();
+        } else if (node.css().shape === 'rectangle') {
+            nodeAdditionalProperties['Agent'] = node.id();
+        }
+        nodeAdditionalProperties['Connection'] = node.id();
+        for (const key in nodeAdditionalProperties) {
+            if (nodeAdditionalProperties.hasOwnProperty(key)) {
+                let value = nodeAdditionalProperties[key];
+                let keyReadable = convertToReadable(key);
+                text = text + `<b>${keyReadable}</b>: ${value}<br/>`;
+            }
+        }
+        $('#highlighter').html(text);
+
+        $('#highlighter').css({
+            // left: (node.position().x + 50) + "px",
+            // top: (node.position().y + 50) + "px",
+            left: '90%',
+            top: '0%',
+            position: 'fixed'
         });
+        $('#highlighter').show();
     });
+
+    cy.on('mouseout', 'node', function (evt) {
+        let node = evt.target;
+        // evt.target.style({
+        //     'background-color': 'gray'
+        // });
+        $('#highlighter').hide();
+    });
+    // window.cy.nodes().map(x => console.log(x.json()));
 }
+
 
 async function fetchGraphData() {
     const response = await fetch("http://localhost:8080/graph", {
@@ -92,19 +145,17 @@ async function fetchGraphData() {
         },
         method: "GET",
         mode: "cors",
-        credentials: "include",
     });
-    const graph = await response.json();
-    return graph;
+    return await response.json();
 }
 
 function getGraph() {
     fetchGraphData().then((graph) => {
-        console.log(graph);
         render(graph);
     }).catch((error) => {
         console.error(error);
     });
 }
 
-getGraph();
+setInterval(getGraph, 1000);
+
